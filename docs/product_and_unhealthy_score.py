f60309d972ba97147eaacbc9e97171656209d8b7
Allansam083 <148643754+Allansam083@users.noreply.github.com>
2024-12-28 17:57:16 +0530

    Add files via upload

---
+++ docs/product_and_unhealthy_score.py
@@ -0,0 +1,5 @@
+prod={'haldirams bhuja':[0.3],'bikano samosa':[0.8],'lays indias magic masala':[0.6],
+ 'parle g biscuit':[1.0],'good day biscuit':[0.8],'bourbon biscuit':[1.0],
+'kissan jam':[1.1],'maggi instant noodles':[1.75],'pepsi':[0.85],'oreo':[1.3],'nutella hazelnut spread':[0.6],'coca cola':[0.7],'pringles original':[1.15]
+,'peanut butter':[0.45],'kitkat':[2.05],'doritos nacho cheese chips':[1.5],'tang drink mix':[0.5],'cheetos flamin hot':[1.1],'perk chocolate bar':[1.25],
+ 'kellogs corn flakes':[0.5]}
