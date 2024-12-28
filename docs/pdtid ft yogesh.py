cea9e198f96976325bd6289964b56bb236697076
Lalitha A R <165548623+lalithaar@users.noreply.github.com>
2024-12-28 18:57:41 +0530

    Create pdtid ft yogesh.py

---
+++ docs/pdtid ft yogesh.py
@@ -0,0 +1,11 @@
+{'product_id':6291007901016,'product_name'="GoodDay Butter biscuits"}
+{'product_id':8901058851311,'product_name'="2 minute maggie"}
+{'product_id':80874997,'product_name'="Kinder joy for girls"}
+{'product_id':8901719101663,'product_name'="Parle G Gold"}
+{'product_id':6291007905311,'product_name'="BOURBON"}
+{'product_id':6001065600048,'product_name'="DAIRY MILK CHOCOLATE"}
+{'product_id':6001065600048,'product_name'="DAIRY MILK CHOCOLATE"}
+{'product_id':8901725181222,'product_name'="Yippe"}
+{'product_id':8904063251060,'product_name'="Haldirams, Soan Cake"}
+{'product_id':786173792989,'product_name'="Haldirams (ready to eat) hyderabadi biryani"}
+
