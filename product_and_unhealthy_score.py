7318fd3859da2541a7ef678ab37144efd5534588
Yogeshwaran <yogeshcyberpro@gmail.com>
2024-12-28 17:10:40 +0530

    Delete product_and_unhealthy_score.py

--- product_and_unhealthy_score.py
+++
@@ -1,4 +0,0 @@
-prod={'haldirams bhuja':[0.3],'bikano samosa':[0.8],'lays indias magic masala':[0.6],
- 'parle g biscuit':[1.0],'good day biscuit':[0.8],'bourbon biscuit':[1.0],
-'kissan jam':[1.1],'maggi instant noodles':[1.75],'pepsi':[0.85],'oreo':[1.3],'nutella hazelnut spread':[0.6]
-}
